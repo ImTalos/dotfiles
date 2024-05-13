7a26ba98b215ec048aafc1bd81977233aea20ded
ImTalos <robot1ba@gmail.com>
2024-05-13 17:01:45 +0300

    adding q! option

--- lua/talos/settings.lua
+++ lua/talos/settings.lua
@@ -2,13 +2,17 @@
 --  editor opt settings
 --]]
 local opt = vim.opt
+-- remove the newline comment
+-- some other settings
 opt.number = true
 opt.relativenumber = true
 opt.mouse = 'a'
 opt.hlsearch = false
+
 opt.breakindent = true
 opt.shiftwidth = 4
 opt.cursorline = true
+
 -- Syncs clipboard between Neovim to OS
 opt.clipboard = 'unnamedplus'
 opt.hlsearch = true
@@ -19,4 +23,5 @@ opt.timeoutlen = 300
 -- Apply theme
 local colorscheme = require("talos.colorscheme")
 vim.cmd.colorscheme(colorscheme)
+
 
